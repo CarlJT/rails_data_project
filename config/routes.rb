c065db2c62744ab6fd84f8d195aa903499f0afa5
Carl Tibule <cjtibule@outlook.com>
2018-10-07 22:59:22 -0500

    Added controller and views for Tag model. Also edited index view of Media model.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  # Show media#index when going to url localhost:3000/media
+  # Media-related routes
   get 'media', to: 'media#index'
   get 'media/:id', to: 'media#show', as: 'medium'
+
+  # Tag-related routes
+  get 'tags', to: 'tags#index'
 end
