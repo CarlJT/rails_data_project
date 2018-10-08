9683c9cd2706ce61f752ff85b3766c2407ec7565
Carl Tibule <cjtibule@outlook.com>
2018-10-07 20:28:59 -0500

    Started to configure routes and the view for Medium model

--- app/controllers/media_controller.rb
+++ app/controllers/media_controller.rb
@@ -1,5 +1,22 @@
 class MediaController < ApplicationController
   def index
+    # By default, index view will only show current season's Media
+    # See article for explanation: https://en.wikipedia.org/wiki/Japanese_television_drama
+
+    # Get current datetime, and find out what the current season is
+    current_year = Time.new.year 
+    current_season = ''
+
+    if Time.new.month  >= 10
+      current_season = 'FALL'
+    elsif Time.new.month >= 7
+      current_season = 'SUMMER'
+    elsif Time.new.month >= 4
+      current_season = 'SPRING'
+    else
+      current_season ='WINTER'
+    end
+
   end
 
   def show
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  get 'media/index'
-  get 'media/show'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  # Show media#index when going to url localhost:3000/media
+  get 'media', to: 'media#index'
 end
